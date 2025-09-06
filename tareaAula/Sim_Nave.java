0ec34c62978a402996bf635f8c427ee17ff51e7e
Daruntu <dal.caceres@proton.me>
2025-09-06 13:29:31 +0000

    simulacion_nave

---
+++ tareaAula/Sim_Nave.java
@@ -0,0 +1,59 @@
+//Una simulación espacial registra naves con nombre, velocidad máxima (km/h),
+//combustible restante (litros), y destino.
+//Los jugadores quieren verificar si una nave puede llegar a su destino (combustible > 1000),
+//acelerar (aumentar velocidad) y mostrar un informe de la misión.
+
+public class Sim_Nave {
+    private String nombre;
+    private double velocidad;
+    private double litros;
+    private String destino;
+
+
+    public Sim_Nave (String nombre, double velocidad, double litros, String destino){
+        this.nombre = nombre;
+        this.velocidad = velocidad;
+        this.litros = litros;
+        this.destino = destino;
+    }
+
+    public boolean puedeLlegar(){
+        return litros > 1000;
+    }
+
+    public void acelerar(){
+        this.velocidad += 100;
+        System.out.println("La nave: " + this.nombre + " aceleró a " + this.velocidad + " km/h");
+    }
+
+    public void mostrarInforme() {
+        System.out.println("-----------------------");
+        System.out.println("Nombre de la nave: " + this.nombre);
+        System.out.println("Destino: " + this.destino);
+        System.out.println("Velocidad: " + this.velocidad + " km/h");
+        System.out.println("Combustible restante: " + this.litros + " litros");
+        System.out.println("-----------------------");
+
+        if (this.puedeLlegar()) {
+            System.out.println("Estado: La nave tiene suficiente combustible para llegar a su destino");
+        }   else {
+            System.out.println("Estado: ¡ALERTA! El combustible es insuficiente para la misión.");
+            }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public double getLitros() {
+        return litros;
+    }
+
+    public String getDetino() {
+        return destino;
+    }
+}    
