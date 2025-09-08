92c1ce1c7afb9534ebb5f6df1d23371df378197d
Daruntu <dal.caceres@proton.me>
2025-09-08 18:49:12 +0000

    ..

--- tareaAula/Simulador.java
+++ tareaAula/Simulador.java
@@ -5,12 +5,15 @@ public class Simulador{
 
         System.out.println("Ingrese el nombre de su nave.");
         String name = scanner.nextLine();
+
         System.out.println("Ingrese la velocidad.");
         double speed = scanner.nextDouble();
         scanner.nextLine();
+
         System.out.println("Ingrese combustible: ");
         double fuel = scanner.nextDouble();
         scanner.nextLine();
+        
         System.out.println("Ingrese el destino: ");
         String destiny = scanner.nextLine();
 
